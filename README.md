6d4cce5f42e10f613f6da022856f6c51343dde1c
isabelaaaguilar <53951492+isabelaaaguilar@users.noreply.github.com>
2022-10-12 11:26:51 -0300

    feat: organização do código e arquivo readme

---
+++ README.md
@@ -0,0 +1,8 @@
+Backend:
+instalar o python na versão 3.10.7 https://www.python.org/downloads/
+$ pip install flask
+$ pip install flask_cors
+$ pip install opencv-python
+$ pip install Pillow
+
+para subir a aplicação utilize $ python app.py
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, send_file, make_response
+from flask import Flask, request, make_response
 from PIL import Image
 from flask_cors import CORS
 import cv2 as cv
@@ -11,52 +11,84 @@ CORS(app)
 
 @app.route("/upload-image", methods=["POST"])
 def home():
+        # Imagem que deseja buscar o corte
+        imageCompare = request.files["imageFiles"] 
 
-        imageCompare = request.files["imageFiles"]        
-         # Imagem que deseja buscar o corte
-       
-        imageCrop = request.files["imageCrop"]
         # Corte de uma imagem
+        imageCrop = request.files["imageCrop"] 
 
+        # Salva o path para a imagem
+        upload_imageCompare_path, upload_imageCrop_path = savePath(imageCompare, imageCrop)
+        
+        # Redimensionando imagem
+        resizeImage(upload_imageCompare_path, imageCompare, upload_imageCrop_path, imageCrop)
+        
+        # Trata imagens em escala de cinza e colorida
+        imageCompareGray, imageCompareColorful, template, w, h = colorAndImageTemplate(imageCompare, upload_imageCrop_path)
+        
+        # faz a correlação cruzada
+        res = cv.matchTemplate(imageCompareGray, template, eval('cv.TM_CCOEFF_NORMED'))
+
+        # pega algumas variaveis para montar o retangulo da area de detecção
+        imageCompareColorful = detectionArea(res, w ,h, imageCompareColorful)
+        
+        # Cria a imagem resultado que será devolvida para o front end da aplicação
+        cv.imwrite("resultado.jpg", imageCompareColorful)
+
+        # Tranforma o file da imagem de resultado numa string na base 64 para retornar para o front
+        with open("resultado.jpg", "rb") as f:
+                image_binary = f.read()
+        response = make_response(base64.b64encode(image_binary))
+
+        #remove os arquivos utilizados no último teste
+        os.remove(imageCompare.filename)
+        os.remove(imageCrop.filename)
+        os.remove("resultado.jpg")
+
+        # retorna a string para o front
+        return response
+
+ # Salva o path para a imagem
+def savePath(imageCompare, imageCrop):
         upload_imageCompare_path = os.path.join(imageCompare.filename)
         imageCompare.save(upload_imageCompare_path)
         upload_imageCrop_path = os.path.join(imageCrop.filename)
         imageCrop.save(upload_imageCrop_path)
+        return upload_imageCompare_path, upload_imageCrop_path
 
-        basewidth = 500 # Redimensionando imagm
+# Redimensionando imagem
+def resizeImage(upload_imageCompare_path, imageCompare, upload_imageCrop_path, imageCrop):
+        basewidth = 500 
         img = Image.open(upload_imageCompare_path)
         wpercent = (basewidth/float(img.size[0]))
         hsize = int((float(img.size[1])*float(wpercent)))
         img=img.resize((basewidth, hsize), PIL.Image.ANTIALIAS)
         img.save(imageCompare.filename)
-
+        
         imgCrop = Image.open(upload_imageCrop_path)
         wpercent = (basewidth/float(imgCrop.size[0]))
         hsize = int((float(imgCrop.size[1])*float(wpercent)))
         imgCrop= imgCrop.resize((basewidth, hsize), PIL.Image.ANTIALIAS)
         imgCrop.save(imageCrop.filename)
 
+# Trata imagens em escala de cinza e colorida
+def colorAndImageTemplate(imageCompare, upload_imageCrop_path):
         imageCompareGray = cv.imread(imageCompare.filename, 0) # Escala de cinza
         imageCompareColorful = cv.imread(imageCompare.filename) # Colorida
-        template = cv.imread(upload_imageCrop_path, 0)
+
+        template = cv.imread(upload_imageCrop_path, 0) #cria o template
         w, h = template.shape[::-1]
+        return imageCompareGray, imageCompareColorful, template, w, h
 
-        # faz a correlação cruzada
-        res = cv.matchTemplate(imageCompareGray, template, eval('cv.TM_CCOEFF_NORMED'))
-
-        # pega algumas variaveis para montar o retangulo da area de detecção
+ # faz a correlação cruzada
+def detectionArea(res, w ,h, imageCompareColorful):
         min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
         top_left = max_loc
         bottom_right = (top_left[0] + w, top_left[1] + h)
-        # monta retangulo na iamgem
+
+        # monta retangulo na imagem
         cv.rectangle(imageCompareColorful,top_left, bottom_right, (0, 0, 255), 2)
 
-        cv.imshow("Image", imageCompareColorful)
-        cv.imwrite("resultado.jpg", imageCompareColorful)
+        return imageCompareColorful
 
-        with open("resultado.jpg", "rb") as f:
-                image_binary = f.read()
-        response = make_response(base64.b64encode(image_binary))
-        return response
-#send_file("resultado.jpg", mimetype='image/jpg')
 app.run(debug=True)

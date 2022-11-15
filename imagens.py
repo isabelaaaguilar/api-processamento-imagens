ba370238fc414d146a066a397a0aa686a410a00e
rossana <rossanaoliveirasouza@gmail.com>
2022-11-15 01:13:49 -0300

    equalização de histograma inicio

--- imagens.py
+++ imagens.py
@@ -1,6 +1,13 @@
 import os
 import shutil
 from PIL import Image
+import cv2 as cv
+import numpy as np
+import matplotlib.image as image
+
+#import matplotlib.pyplot as plt 
+#from skimage import data
+
 
 
 def renomeiaESalva():
@@ -22,7 +29,25 @@ def renomeiaESalva():
             newDestination = newFolder + newFileName
             shutil.copy(destination, newDestination)
 
+            # faz a transposição da imagem
             transposeImage(destination, folder, newFolder, x, count)
+
+
+
+
+            # carrega a imagem em memória principal com a biblioteca openCv
+            imagem = image.imread(destination)
+            #imagem = data.coffee()
+
+            # Inicia o histograma - objeto com 255 valores num dicionário
+            histograma = instantiate_histogram()
+            # Quanta vezes cada valor de intensidade do histograma aparece na imagem
+            histograma = count_intensity_values(histograma, imagem)
+
+            n_pixels = imagem.shape[0] * imagem.shape[1]
+            hist_proba = get_hist_proba(histograma, n_pixels)
+
+
             
             count += 1
 
@@ -38,6 +63,32 @@ def transposeImage(destination, folder, newFolder, x, count):
     # faz a cópia para a pasta grau artrose
     newDestination = newFolder + transposeFileName
     shutil.copy(destination, newDestination)
+
+def instantiate_histogram():
+    hist_array = []
+    
+    for i in range(0,256):
+        hist_array.append(str(i))
+        hist_array.append(0)
+    
+    hist_dct = {hist_array[i]: hist_array[i + 1] for i in range(0, len(hist_array), 2)} 
+    
+    return hist_dct
+
+def count_intensity_values(hist, img):
+    print(img)
+    for row in img:
+       for column in row:
+            hist[str(int(column))] = hist[str(int(column))] + 1
+     
+    return hist
+
+def get_hist_proba(hist, n_pixels):
+    hist_proba = {}
+    for i in range(0, 256):
+        hist_proba[str(i)] = hist[str(i)] / n_pixels
+    
+    return hist_proba
 
 
 def main():
